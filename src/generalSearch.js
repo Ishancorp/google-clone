7e5b74b58d7f27ccdd93d93f1f2bac3495cd04f6
Ishancorp <sharma.ishan450@gmail.com>
2023-08-14 20:42:41 -0700

    Fixing styling

--- src/generalSearch.js
+++ src/generalSearch.js
@@ -39,7 +39,7 @@ function Main() {
                         <svg className="searchglass" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path fill="#9aa0a6" d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                         </svg>
-                        <textarea rows="1" cols="80" expanded="false"></textarea>
+                        <textarea className="index" rows="1" cols="80" expanded="false"></textarea>
                         <svg className="colored" focusable="false" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path fill="#4285f4" d="m12 15c1.66 0 3-1.31 3-2.97v-7.02c0-1.66-1.34-3.01-3-3.01s-3 1.34-3 3.01v7.02c0 1.66 1.34 2.97 3 2.97z"></path>
                             <path fill="#34a853" d="m11 18.08h2v3.92h-2z"></path>
--- src/results.js
+++ src/results.js
@@ -18,7 +18,7 @@ export default function Results() {
                 <div className="topnotlogo">
                     <div className="topmost">
                         <div className="textbox-res">
-                            <textarea rows="1" cols="10" expanded="false"></textarea>
+                            <textarea className="res" rows="1" cols="10" expanded="false"></textarea>
                             <svg className="colored" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path fill="#9aa0a6" d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
                             </svg>
